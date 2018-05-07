b621caeff7a9fac5d69f574b1cfdef4c1971c74f
Padellus <philepatti@gmail.com>
2018-05-07 12:57:15 -0400

    start TankAimingComponent

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -22,6 +23,9 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Called to bind functionality to input
