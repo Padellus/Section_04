2dee9676f8b9c08e4fa72d640b918602a113f082
Padellus <philepatti@gmail.com>
2018-05-07 14:43:40 -0400

    fix intellisense

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "../Public/TankAimingComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "TankAimingComponent.h"
-
 
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
