4e684dd60d70f0819d2150c75fa6487736d3fb38
Padellus <philepatti@gmail.com>
2018-05-09 23:35:49 -0400

    ProjectileMovementComponent

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -55,9 +55,11 @@ void ATank::Fire()
 	if (!Barrel) { return; }
 
 	// Spawn projectile at barrel socket location
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 	);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
