654d30bf1ad49077c360d273c131ef9dce2ef3ff
Padellus <philepatti@gmail.com>
2018-05-10 15:43:14 -0400

    throttle inputs

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	// TODO Clamp actual throttle
+}
+
