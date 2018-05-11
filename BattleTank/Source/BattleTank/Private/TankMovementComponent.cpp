a5c131b3128c799bfd315a1cac506ae635f7b3fc
Padellus <philepatti@gmail.com>
2018-05-11 00:13:34 -0400

    clean up blueprint callable

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,10 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Public/TankMovementComponent.h"
+#include "Public/TankTrack.h"
 
+
+void UTankMovementComponent::Intialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (LeftTrackToSet) {
+		LeftTrack = LeftTrackToSet;
+	}
+	if (RightTrackToSet) {
+		RightTrack = RightTrackToSet;
+	}
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("IntendMoveForward Throw: %f"), Throw)
+	UE_LOG(LogTemp, Warning, TEXT("IntendMoveForward Throw: %f"), Throw);
+	if (LeftTrack) {
+		LeftTrack->SetThrottle(Throw);
+	}
+	if (RightTrack) {
+		RightTrack->SetThrottle(Throw);
+	}
+	// TODO prevent double speed
 }
 
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,10 +6,12 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -18,5 +20,12 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Intialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 	
+private:
+	UTankTrack* LeftTrack = nullptr;
+
+	UTankTrack* RightTrack = nullptr;
 };
