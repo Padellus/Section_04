f86a88f02e9f0c04638ee3fc68dcbaff8e7e157c
Padellus <philepatti@gmail.com>
2018-05-17 23:03:40 -0400

    ai controller bp

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Phillip Patti.
 
 #pragma once
 
@@ -15,11 +15,14 @@ class BATTLETANK_API ATankAIController : public AAIController
 	GENERATED_BODY()
 	
 public:
+
+protected:
+	// How close can the AI tank get to the player
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float AcceptanceRadius = 8000;
+
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
-
-private:
-	// How close can the AI tank get to the player
-	float AcceptanceRadius = 3000;
 };
